621eb855df223bdaa2ad81f6097827551baa7d28
unknown <punithmatli@gmail.com>
2020-05-02 17:18:07 +0530

    Added Toolbar for navigation and Logo

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import Auxiliary from '../../hoc/Auxiliary'
 import classes from './Layout.css'
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+
 const Layout = (props) => {
     return <Auxiliary>
-        <div>
-        Toolbar, Sidebar, Backdrop
-    </div>   
+        <Toolbar />  
     <main className={classes.Content}>
         {props.children}
     </main></Auxiliary>
