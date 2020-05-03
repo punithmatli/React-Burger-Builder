c225d05bd28ff086c172a1a4e8a0d3a3adaf20bd
unknown <punithmatli@gmail.com>
2020-05-03 12:40:04 +0530

    Added navigation items on top

---
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './NavigationItem.css'
+
+const NavigationItem = (props) => {
+    return <li className={classes.NavigationItem}>
+            <a 
+            href="/"
+            className={props.active ? classes.active : null} >{props.children}</a>
+        </li>
+}
+
+export default NavigationItem
