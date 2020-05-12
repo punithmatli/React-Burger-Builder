2a8fddae375537f545368192a5f57ced6e63a2a6
unknown <punithmatli@gmail.com>
2020-05-12 19:27:02 +0530

    Display error message when something goes wrong

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Modal from '../../components/UI/Modal/Modal'
 import Auxiliary from '../Auxiliary/Auxiliary'
 
-const withErrorHandler = (WrappedComponent) => {
-    return (props) => {
-        return (
-            <Auxiliary>
-                <Modal show>Something went wrong!</Modal>
-                <WrappedComponent {...props} />
-            </Auxiliary>
-            
-        )
-    }
+const withErrorHandler = (WrappedComponent, axios) => {
+    return class extends Component {
+        state= {
+            error:null
+        }
+        
+        componentDidMount(){
+            axios.interceptors.request.use(req => {
+                this.setState({error:null})
+                return req;
+            })
+            axios.interceptors.response.use(res => res,error => {
+                this.setState({error:error})
+
+            })
+        }
+
+        errorConfirmedHandler = () => {
+            this.setState({error:null})
+        }
+
+        render(){
+            return <Auxiliary>
+            <Modal show={this.state.error} modalclicked={this.errorConfirmedHandler}>{this.state.error ? this.state.error.message : null}</Modal>
+            <WrappedComponent {...this.props} />
+        </Auxiliary>
+        }
+    } 
 }
 
 export default withErrorHandler;
