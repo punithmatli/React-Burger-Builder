a37206f8a8a784a954afb04827949ed7f27da477
unknown <punithmatli@gmail.com>
2020-05-25 19:02:00 +0530

    Added validation feedback

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -18,7 +18,8 @@ class ContactData extends Component {
                     validation: {
                         required:true
                     },
-                    valid:false
+                    valid:false,
+                    touched:false
                 },
                     street:{
                         elementType:'input',
@@ -30,7 +31,8 @@ class ContactData extends Component {
                         validation: {
                             required:true
                         },
-                        valid:false
+                        valid:false,
+                        touched:false
                     },
                     zipCode:{
                         elementType:'input',
@@ -44,7 +46,8 @@ class ContactData extends Component {
                             minLength:5,
                             maxLength:6
                         },
-                        valid:false
+                        valid:false,
+                        touched:false
                     },
                     country:{
                         elementType:'input',
@@ -56,7 +59,8 @@ class ContactData extends Component {
                         validation: {
                             required:true
                         },
-                        valid:false
+                        valid:false,
+                        touched:false
                     },    
                 email:{
                     elementType:'input',
@@ -68,7 +72,8 @@ class ContactData extends Component {
                     validation: {
                         required:true
                     },
-                    valid:false
+                    valid:false,
+                    touched:false
                 },
             deliveryMethod:{
                 elementType:'select',
@@ -136,6 +141,7 @@ class ContactData extends Component {
         }
         updatedFormElement.value = event.target.value;
         updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation)
+        updatedFormElement.touched = true;
         updatedOrderForm[inputIdentifier] = updatedFormElement;
         console.log(updatedFormElement)
         this.setState({
@@ -159,6 +165,9 @@ class ContactData extends Component {
                     elementType={formElement.config.elementType}
                     elementConfig={formElement.config.elementConfig}
                     value={formElement.config.value}
+                    invalid={!formElement.config.valid}
+                    touched={formElement.config.touched}
+                    shouldValidate={formElement.config.validation}
                     changed={(event) => this.inputChangedHandler(event, formElement.id)} />
             ))}
             <Button btnType="Success">ORDER</Button>
