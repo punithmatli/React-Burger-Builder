c68d5c1cdd338deb29e4cf95bdc9a93c7c919b2e
unknown <punithmatli@gmail.com>
2020-06-04 13:16:33 +0530

    Added an Authentication form

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -6,6 +6,7 @@ const NavigationItems = (props) => {
     return <ul className={classes.NavigationItems}>
         <NavigationItem exact link='/'>Burger Builder</NavigationItem>
         <NavigationItem link='/orders'>Orders</NavigationItem>
+        <NavigationItem link='/auth'>Authenticate</NavigationItem>
     </ul>
 }
 
