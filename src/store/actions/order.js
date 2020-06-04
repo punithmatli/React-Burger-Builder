a7609f21d1b98df308b81e53b55b28c5e365d64d
unknown <punithmatli@gmail.com>
2020-06-04 12:14:37 +0530

    Fetched orders using redux and refactored reducers

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -18,7 +18,7 @@ export const purchaseBurgerFail = (error) => {
 
 export const purchaseBurgerStart = () => {
     return {
-        type:actionTypes.PURCHASE_BURGER_START
+        type: actionTypes.PURCHASE_BURGER_START
     }
 }
 
@@ -37,6 +37,46 @@ export const purchaseBurger = (orderData) => {
 
 export const purchaseInit = () => {
     return {
-        type:actionTypes.PURCHASE_INIT
+        type: actionTypes.PURCHASE_INIT
+    }
+}
+
+export const fetchOrdersSuccess = (orders) => {
+    return {
+        type: actionTypes.FETCH_ORDERS_SUCCESS,
+        orders: orders
+    }
+}
+
+export const fetchOrdersFail = (error) => {
+    return {
+        type: actionTypes.FETCH_ORDERS_FAIL,
+        error: error
+    }
+}
+
+export const fetchOrdersStart = () => {
+    return {
+        type: actionTypes.FETCH_ORDERS_START
+    }
+}
+
+export const fetchOrders = () => {
+    return dispatch => {
+        dispatch(fetchOrdersStart());
+        axios.get('/orders.json')
+            .then(res => {
+                let fetchedOrders = [];
+                for (let key in res.data) {
+                    fetchedOrders.push({
+                        ...res.data[key],
+                        id: key
+                    });
+                }
+                dispatch(fetchOrdersSuccess(fetchedOrders))
+            })
+            .catch(err => {
+                dispatch(fetchOrdersFail(err))
+            })
     }
 }
